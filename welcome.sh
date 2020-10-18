1ac7ba51a2064518180bae8fcf8c24f1a2074b72
“Enter <merin@gmail.com>
2020-10-18 21:43:29 +0530

    sixth commit

--- welcome.sh
+++ welcome.sh
@@ -64,31 +64,27 @@ esac
 
 
 
+
 #! /bin/bash
+wage_per_hr=20
+full_day_hr=8
 working_days=20
-per_hr=20
-full_day=8
+randomCheck=$(( RANDOM%2 ))
+
 echo "enter choice"
 read n
 case $n in
 0)
- echo "not available"
+echo "not completed 20 days"
 ;;
 1)
-for((i=1;i<=working_days;i++))
-do
-
-wage=$(( $full_day * $per_hr ))
-total=$(( $wage * $i ))
-i=$(( $i + 1 ))
-done
+echo "present"
+wage=$(( $wage_per_hr * $full_day_hr * $working_days ))
+echo "$wage"
 ;;
 
 *)
 echo "wrong"
 ;;
 esac
-
-
-echo "total wage he or she completed 20 days or 100hr:$total"
 
