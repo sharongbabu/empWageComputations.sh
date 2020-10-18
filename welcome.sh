7fe64d98487ddbc846429664e71ee032c50aa6cf
“Enter <merin@gmail.com>
2020-10-18 20:21:47 +0530

    my third commit

--- welcome.sh
+++ welcome.sh
@@ -13,3 +13,16 @@ echo "employee is absent"
 fi
 
 
+#! /bin/bash
+wage_per_hr=20
+full_day_hr=8
+isPresent=1
+randomCheck=$(( RANDOM%2 ))
+if [ $isPresent -eq $randomCheck ]
+then
+salary=$(( $wage_per_hr * $full_day_hr ))
+echo "$salary"
+else
+echo "employee is absent"
+fi
+
